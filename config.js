ce8122794f3686e415f8c3620e0bb5577f6004e1
seongyu <suengyu@me.com>
2016-09-26 11:04:45 +0900

    commit for port

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 process.env.NODE_ENV = process.env.NODE_ENV? process.env.NODE_ENV : 'development';
 
 exports.parkinglotSeq = process.env.parkinglot? process.env.parkinglot : '111810';
-exports.port = process.env.port? process.env.port : 9001;
+exports.port = process.env.port? process.env.port : 80;
 
 exports.targetDNS = 'http://52.68.215.38:9001/';
 exports.mongodb = 'mongodb://52.68.215.38:27017/iot-mongo';
